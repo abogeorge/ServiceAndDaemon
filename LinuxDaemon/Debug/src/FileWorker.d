ef5b06c5858167d9cc487d6479af385792c41c24
George Abordioaie <abordioaie.george@yahoo.com>
2015-08-01 10:13:07 +0300

    Removed unnecessary files and projects

--- LinuxDaemon/Debug/src/FileWorker.d
+++
@@ -1,6 +0,0 @@
-src/FileWorker.d: ../src/FileWorker.cpp ../src/stdafx.h \
- ../src/FileWorker.h
-
-../src/stdafx.h:
-
-../src/FileWorker.h:
--- LinuxDaemon/Debug/src/LinuxDaemon.d
+++
@@ -1,6 +0,0 @@
-src/LinuxDaemon.d: ../src/LinuxDaemon.cpp ../src/stdafx.h \
- ../src/FileWorker.h
-
-../src/stdafx.h:
-
-../src/FileWorker.h:
